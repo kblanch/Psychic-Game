798204934918904a82017b57858fe8d6760c0280
Karen <K.Blanchette07@gmail.com>
2017-05-21 13:15:19 -0500

    Working Code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,14 @@
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var index = getRandomInt(alphabet.length);
+var index = getRandomIndex(alphabet.length);
 var letter = alphabet[index];
 var guessesLeft = 9;
+var guessList = [];
 var gamesWon = 0;
 var gamesLost = 0;
-var guessList = [];
-var gameWon = false;
-
-function getRandomInt(x) {
- 	return Math.floor(Math.random() * x);
-}
 
 document.addEventListener("DOMContentLoaded", function(event) {
-	var ltr = document.getElementById("letter"); 
-	ltr.textContent = letter;
+	//var ltr = document.getElementById("letter"); 
+	//ltr.textContent = letter;
 	var wns = document.getElementById("wins"); 
 	wns.textContent = gamesWon;
 	var los = document.getElementById("losses"); 
@@ -26,58 +21,50 @@ document.addEventListener("DOMContentLoaded", function(event) {
 document.onkeypress = function(e){
 	var userInput = e.key;
 
-	if(isAlpha(userInput)){
+	if(isAlpha(userInput) && newGuess(userInput)){//Is user input an alpha character and has it been guessed?
 		if(userInput === letter){//Game Won
-			//gameWon = true;
 			gamesWon++;
-			console.log(gamesWon);
-			alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-			index = getRandomInt(alphabet.length);
-			letter = alphabet[index];
-			guessesLeft = 9;
-			guessList = [];
-			gameWon = false;
-			ltr = document.getElementById("letter"); 
-			ltr.textContent = letter;
-			wns = document.getElementById("wins"); 
-			wns.textContent = gamesWon;
-			los = document.getElementById("losses"); 
-			los.textContent = gamesLost;
-			guessesLft = document.getElementById("guesses-left"); 
-			guessesLft.textContent = guessesLeft;
-			lst = document.getElementById("guess-list"); 
-			lst.textContent = guessList;
+			alert("You guessed the correct letter: " + letter);
+			newGame();
 		}
 		else if(guessesLeft === 1){//Game Lost
-			//gameWon stays = false
 			gamesLost++;
-			alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-			index = getRandomInt(alphabet.length);
-			letter = alphabet[index];
-			guessesLeft = 9;
-			guessList = [];
-			gameWon = false;
-			ltr = document.getElementById("letter"); 
-			ltr.textContent = letter;
-			wns = document.getElementById("wins"); 
-			wns.textContent = gamesWon;
-			los = document.getElementById("losses"); 
-			los.textContent = gamesLost;
-			guessesLft = document.getElementById("guesses-left"); 
-			guessesLft.textContent = guessesLeft;
-			lst = document.getElementById("guess-list"); 
-			lst.textContent = guessList;
+			alert("The correct letter was: " + letter);
+			newGame();
 		}
 		else {//Continue Game
 		guessesLeft--;
+		guessList.push(userInput);
 		guessesLft = document.getElementById("guesses-left"); 
 		guessesLft.textContent = guessesLeft;
-		guessList.push(userInput);
 		lst = document.getElementById("guess-list"); 
 		lst.textContent = guessList;
 		}
 
 	}
+}
+
+function getRandomIndex(x){
+ 	return Math.floor(Math.random() * x);
+}
+
+function newGame(){
+	alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+	index = getRandomIndex(alphabet.length);
+	letter = alphabet[index];
+	guessesLeft = 9;
+	guessList = [];
+
+	//ltr = document.getElementById("letter"); 
+	//ltr.textContent = letter;
+	wns = document.getElementById("wins"); 
+	wns.textContent = gamesWon;
+	los = document.getElementById("losses"); 
+	los.textContent = gamesLost;
+	guessesLft = document.getElementById("guesses-left"); 
+	guessesLft.textContent = guessesLeft;
+	lst = document.getElementById("guess-list"); 
+	lst.textContent = guessList;
 }
 
 function isAlpha(l){
@@ -91,11 +78,13 @@ function isAlpha(l){
 	return alpha;
 }
 
-// function newGuess(l){
-// 	var newInput = false;
-// 	for (var i = 0; i < guessList; i++){
-// 		if(l === guessList[i]){
-			
-// 		}
-// 	}
-// }
+ function newGuess(l){
+ 	var newInput = true;
+ 	for (var i = 0; i < guessList.length; i++){
+ 		if(l === guessList[i]){
+			newInput = false;
+			i = guessList.length
+ 		}
+ 	}
+ 	return newInput;
+}
